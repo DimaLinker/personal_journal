036974c821f925c06f28565709067283aea5d0ee
Dima Linker <daniel.linker@mailbox.org>
2024-01-01 23:52:44 +0100

    ordner erstellt

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,15 @@
 import "./Button.css";
+import { useState } from "react";
 export const Button = () => {
+  const [text, setText] = useState("Ausgabe");
+  const click = () => {
+    setText("zumachen");
+  };
   return (
     <>
-      <button className="button accent">Hinzufügennnn</button>
+      <button className="button accent" onClick={click}>
+        {text}
+      </button>
     </>
   );
 };
